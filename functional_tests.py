5b8c67ced5c66d28727b5e11c4653fd7aa3374af
Felix Schlösser <git@felixschloesser.de>
2023-10-19 20:11:41 +0200

    Add app for lists, with deliberately failing unit test

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,20 @@
+import unittest
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get("http://localhost:8000")
 
-assert "Congratulations!" in browser.title
-print("OK")
+class NewVisitortest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+
+    def test_can_start_a_doto_list(self):
+        self.browser.get("http://localhost:8000")
+        self.assertIn("To-Do", self.browser.title)
+
+        self.fail("Finish the test!")
+
+
+if __name__ == "__main__":
+    unittest.main(warnings="ignore")
