be28d0db7ae8fba058600d126a430afc0ca0fe82
Felix Schlösser <git@felixschloesser.de>
2023-10-20 14:19:36 +0200

    Refactored home page to use a template

--- lists/views.py
+++ lists/views.py
@@ -6,4 +6,4 @@ from django.http import HttpResponse
 
 
 def home_page(request) -> HttpResponse:
-    return HttpResponse("<html><title>To-Do lists</title></html>")
+    return render(request, "home.html")
