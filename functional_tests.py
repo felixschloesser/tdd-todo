f5daf506c4bc7180559df5915c76a842d5e2d87d
Felix Schlösser <git@felixschloesser.de>
2023-10-21 14:03:59 +0200

    refactored FT

--- functional_tests.py
+++ functional_tests.py
@@ -13,6 +13,11 @@ class NewVisitortest(unittest.TestCase):
     def tearDown(self) -> None:
         self.browser.quit()
 
+    def check_for_row_in_table(self, row_text):
+        table = self.browser.find_element(By.ID, "list_table")
+        rows = table.find_element(By.TAG_NAME, "tr")
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_doto_list(self):
         # Edith has heard about a cool new online to-do app.
         # She dies to check out its home page
@@ -37,17 +42,21 @@ class NewVisitortest(unittest.TestCase):
         # "1: Buy peacock feathers" as an item in a to-do lists
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.browser.find_element(By.ID, "item_list")
-        rows = table.find_elements(By.TAG_NAME, "tr")
-
-        rows_contains_text = any(row.text == "1: Buy peacock feathers" for row in rows)
-        self.assertTrue(rows_contains_text, "New to-do item did not appear in table")
+        self.check_for_row_in_table("1: Buy peackock feathers")
 
         # The is still a text box inviting her to add another item.
         # She enters "Use peacock feathers to make a fly"
         # (Edith is very methodical)
-        self.fail("Finish the test!")
+        inputbox = self.browser.find_element(By.ID, "new_item")
+        inputbox.send_keys("Use peacock feathers to make a fly")
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        # The page updates again, and now shwos both items on her list
+        self.check_for_row_in_table("1: Buy pecock feathres")
+        self.check_for_row_in_table("2: Use peacock feathers to make a fly")
+
+        # Satisfied, she goes back to sleep
 
 
 if __name__ == "__main__":
