79880b0eafaaf73550639bbfd6f0321937d343da
Felix Schlösser <git@felixschloesser.de>
2023-11-02 23:16:48 +0100

    added hack - set ALLOWED_HOSTS to *

--- superlists/settings.py
+++ superlists/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "changeme")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
-
+# ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
