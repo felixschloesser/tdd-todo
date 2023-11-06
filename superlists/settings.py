eeda576fafe68f26199fa04aa9f3bc59ea32f5bd
Felix Schlösser <git@felixschloesser.de>
2023-11-06 12:34:31 +0100

    changed to specific CSRF origins

--- superlists/settings.py
+++ superlists/settings.py
@@ -28,7 +28,10 @@ DEBUG = True
 
 # ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ["http://*"]
+CSRF_TRUSTED_ORIGINS = [
+    "http://todo.staging.felixschloesser.de",
+    "http://localhost:8000",
+]
 
 # Application definition
 
